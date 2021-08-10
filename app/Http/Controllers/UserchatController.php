426750676ee9e21b749f36236a11238554ab55e9
mixmaster429 <mjone352@gmail.com>
2021-08-10 08:51:44 +0800

    message notification filter enable

--- app/Http/Controllers/UserchatController.php
+++ app/Http/Controllers/UserchatController.php
@@ -11,98 +11,111 @@ use App\Models\UserChat;
 use App\Mail\MessageMail;
 use Mail;
 use Log;
+use App\Models\NotificationFilter;
 
 class UserchatController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
 
-    public function fetchMessages(Request $request)
-    {
-        $sender = $request->get('sender');
-        $receiver = $request->get('receiver');
-        // sender = user_id
-        // receiver = user_with
-        $messages = Userchat::with('user')->whereIn('user_id', [$receiver, $sender])->whereIn('user_to', [$receiver, $sender])->where('deleted_by', '<>', $sender)->get();
-        $first_unread_flag = 0;
-        foreach ($messages as $message) {
-            if($message->read == 0 && $first_unread_flag == 0){
-                $message->first_unread = 1;
-                $first_unread_flag = 1;
-            }else{
-                $message->first_unread = 0;
-            }
-            if ($message->user_id == $sender) {
-                $message->type = 1;
-            } else {
-                $message->type = 0;
-            }
-        }
-
-        return $messages;
+  public function fetchMessages(Request $request)
+  {
+    $sender = $request->get('sender');
+    $receiver = $request->get('receiver');
+    // sender = user_id
+    // receiver = user_with
+    $messages = Userchat::with('user')->whereIn('user_id', [$receiver, $sender])->whereIn('user_to', [$receiver, $sender])->where('deleted_by', '<>', $sender)->get();
+    $first_unread_flag = 0;
+    foreach ($messages as $message) {
+      if ($message->read == 0 && $first_unread_flag == 0) {
+        $message->first_unread = 1;
+        $first_unread_flag = 1;
+      } else {
+        $message->first_unread = 0;
+      }
+      if ($message->user_id == $sender) {
+        $message->type = 1;
+      } else {
+        $message->type = 0;
+      }
     }
 
-    public function sendMessage(Request $request)
-    {
-        $user = auth()->user();
+    return $messages;
+  }
 
-        $created_at = $user->userchat()->create([
-            'user_to' => $request->input('receiver'),
-            'message' => $request->input('message')
-        ])->created_at;
+  public function sendMessage(Request $request)
+  {
+    $user = auth()->user();
 
-        $receiver = User::find($request->input('receiver'));
+    $created_at = $user->userchat()->create([
+      'user_to' => $request->input('receiver'),
+      'message' => $request->input('message')
+    ])->created_at;
 
+    $receiver = User::find($request->input('receiver'));
+    $sender_name = $user->name;
+    $sender_link = $user->username;
+    $receiver_name = $receiver->name;
+    $receiver_link = $receiver->username;
+    $receiver_email = $receiver->email;
 
-        $sender_name = $user->name;
-        $sender_link = $user->username;
-        $receiver_name = $receiver->name;
-        $receiver_link = $receiver->username;
-        $receiver_email = $receiver->email;
+    $notification_filters = NotificationFilter::where('user_id', $receiver->id)->first();
 
+    if ($notification_filters) {
+      $notification_filters = $notification_filters->value;
+      $filter_array = explode(", ", $notification_filters);
+      if (in_array("message", $filter_array)) {
         try {
-            Mail::to($receiver_email)->send(new MessageMail($sender_name, $sender_link, $receiver_name, $receiver_link));
+          Mail::to($receiver_email)->send(new MessageMail($sender_name, $sender_link, $receiver_name, $receiver_link));
         } catch (\Throwable $th) {
-            //throw $th;
-            Log::error($th);
+          Log::error($th);
         }
-
-        return ['status' => true, 'created_at' => $created_at];
+      }
+    } else {
+      try {
+        Mail::to($receiver_email)->send(new MessageMail($sender_name, $sender_link, $receiver_name, $receiver_link));
+      } catch (\Throwable $th) {
+        Log::error($th);
+      }
     }
 
-    public function imageupload(Request $request, $sender, $receiver) {
-        $imageables = ['jpg', 'JPG', 'jpeg', 'png', 'gif'];
-        $attachment = request()->file('file');
+    return ['status' => true, 'created_at' => $created_at];
+  }
 
-        $imageName = time() . '.' . $attachment->getClientOriginalExtension();
-        $attachment->move(public_path('uploaded/chat'), $imageName);
+  public function imageupload(Request $request, $sender, $receiver)
+  {
+    $imageables = ['jpg', 'JPG', 'jpeg', 'png', 'gif'];
+    $attachment = request()->file('file');
 
-        $user = User::find($sender);
+    $imageName = time() . '.' . $attachment->getClientOriginalExtension();
+    $attachment->move(public_path('uploaded/chat'), $imageName);
 
-        $fileurl = '/uploaded/chat/' . $imageName;
+    $user = User::find($sender);
 
-        $created_at = $user->userchat()->create([
-            'user_to' => $receiver,
-            'message' => $fileurl,
-            'file' => 1
-        ])->created_at;
+    $fileurl = '/uploaded/chat/' . $imageName;
 
-        return ['status' => true, 'created_at' => $created_at, 'url' => $fileurl];
+    $created_at = $user->userchat()->create([
+      'user_to' => $receiver,
+      'message' => $fileurl,
+      'file' => 1
+    ])->created_at;
+
+    return ['status' => true, 'created_at' => $created_at, 'url' => $fileurl];
+  }
+
+  public function readall(Request $request)
+  {
+    $loggeduser = auth()->id();
+    $receiver = $request->get('receiver');
+    $userchat = Userchat::where('user_id', $receiver)->where('user_to', $loggeduser)->get();
+    foreach ($userchat as $chat) {
+      $chat->read = 1;
+      $chat->save();
     }
 
-    public function readall(Request $request)
-    {
-        $loggeduser = auth()->id();
-        $receiver = $request->get('receiver');
-        $userchat = Userchat::where('user_id', $receiver)->where('user_to', $loggeduser)->get();
-        foreach ($userchat as $chat){
-            $chat->read = 1;
-            $chat->save();
-        }
-
-        return ['status' => true];
-    }
+    return ['status' => true];
+  }
 }
